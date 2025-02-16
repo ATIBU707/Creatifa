a108c26865460a81a331c23d3a577e030bce1d6c
ATIBU707 <wasswaatibu@gmail.com>
2025-02-16 15:30:30 +0300

    changes in the header for logo

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,20 @@
 </head>
 <body>
     <header>
-        <nav id="main-menu">
+        <nav id="main">
+            <div class="logo">
+                <img src="./images/logo.webp" alt="logo">
+                <h2>Creatifa</h2>
+            </div>
+            <ul class="main-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+        <nav id="mobile-menu">
             <ul class="main-menu">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
